11275f785d58b1059018f987752a37467ac85b87
k-i-r-a-n-ch <kiran.162011@gmail.com>
2025-05-17 12:16:56 +0530

    adding files

---
+++ ec2/ec2.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+    name = "allow_ssh"
+    description = "Allow port number 22 for SSH Access"
+
+    # usually we allow everything in egress
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] # allow from everyone
+        ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+      Name = "allow_ssh"
+  }
+}
+
+resource "aws_instance" "terraform" {
+
+      ami = "ami-0754490447848"
+      instance_type = "t3.micro"
+      vpc_security_group_ids =[aws_security_group.allow_ssh_terraform.id]
+
+      tags = {
+      Name = "terraform"
+  }
+
+}
